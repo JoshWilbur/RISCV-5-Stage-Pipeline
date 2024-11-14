f4497ce9bf35a206ccc8583be52493616bd15db4
JoshWilbur <joshuawilbur03@gmail.com>
2024-11-14 17:37:21 -0500

    upload milestone 3 & 4 files

---
+++ Milestone-3/Milestone-3b-beq.asm
@@ -0,0 +1,24 @@
+.text	
+main:         
+	ori x6, x0, 1
+	ori x7, x0, 2
+	nop
+	nop		
+	beq x6, x7, L
+	nop
+	nop
+	nop		
+	ori x10, x0, -1
+    	ori x11, x0, -1
+   	ori x12, x0, -1
+    	ori x13, x0, -1
+L:  	ori x10, x0, 1
+    	ori x11, x0, 1
+    	ori x12, x0, 1
+	ori x13, x0, 1
+	nop
+	nop
+	nop
+	nop
+	nop
+
